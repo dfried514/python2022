a92b51895ef52213a6d7fee0bfefa1ad799ecc93
dfried514 <dfried514@gmail.com>
2022-06-04 16:17:11 -0700

    intermediate functions

---
+++ fundamentals/fundamentals/functions_intermediate_i.py
@@ -0,0 +1,79 @@
+x = [ [5,2,3], [10,8,9] ]
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+print(x)
+
+students[0]['last_name'] = 'Bryant'
+print(students)
+
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
+
+z[0]['y'] = 30
+print(z)
+
+def iterateDictionary(some_list):
+    for person in some_list:
+        print(f"first_name - {person['first_name']}, last_name - {person['last_name']}")
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+iterateDictionary(students)
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+def iterateDictionary2(key_name, some_list):
+    for item in some_list:
+        if key_name in item:
+            print(item[key_name])
+
+iterateDictionary2('last_name', students)
+
+def printInfo(some_dict):
+    for key,values in some_dict.items():
+        print('--------------------')
+        print(len(values), key.upper())
+        for value in values:
+            print(value)
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
+# # output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+#
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
